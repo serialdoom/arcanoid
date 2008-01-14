ca8e05c8fa0136529f645b27b9866cfac942405f
koutsop <koutsop@5c4dd20e-9542-0410-abe3-ad2d610f3ba4>
2008-01-14 11:02:17 +0000

    Kainouria arxeia: LoadFilmsInfo.h LoadFilmsInfo.cpp BitmapLoader.h BitmapLoader.cpp
    Pira3a ta:
    films.cfg
    AnimationFilmHolder.h
    
    8a ginoun kai ales alages sta arxeia BitmapLoad kai AnimationFilmHolder

---
+++ Arcanoid/Arcanoid/BitmapLoader.cpp
@@ -0,0 +1,26 @@
+#include "BitmapLoader.h"
+/*
+BitmapLoader::BitmapLoader(void)
+{
+}
+
+BitmapLoader::~BitmapLoader(void)
+{
+}
+
+
+BITMAP * BitmapLoader::GetBitmap(const string path) const {
+		BitmapMap::const_iterator i = bitmaps.find(path);
+		return( i != bitmaps.end() ? i->second : (Bitmap) 0 );
+}
+
+
+BITMAP * BitmapLoader::Load (const std::string& path) {
+	BITMAP * b = GetBitmap(path);
+	if (!b) {
+		bitmaps[path] = ( b = LoadBitmap(path) );
+		assert(b);
+	}
+	return b;
+}
+*/

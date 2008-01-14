96d6c854f35d7f583461bbb49922d4db2fff2088
koutsop <koutsop@5c4dd20e-9542-0410-abe3-ad2d610f3ba4>
2008-01-14 12:29:59 +0000

    Teleiase h LoadFilmInfo. Episeis eginan alages sthn main

--- trunk/Arcanoid/Arcanoid/LoadFilmsInfo.h
+++ trunk/Arcanoid/Arcanoid/LoadFilmsInfo.h
@@ -6,21 +6,35 @@
 #define LOADFIMLSINFO_H
 
 #include <map>
+#include <string>
 #include <cassert>
+
 using std::map;
 using std::string;
+using std::make_pair;
 
+typedef map< string, std::pair<string, string> > FilmsInfoMap;
 
 class LoadFilmsInfo
 {
 private:
-	typedef map<string, map<string, string>> FilmsMap;
-	FilmsMap filmsMap;
+	FilmsInfoMap filmsInfo;
 	int filmsNo;
 	
+	/* @target: Na diavazei ena config file
+	 */
 	void LoadInfo(void);
 
 public:
+	
+	/* @return: Twn ari8mo twn films
+	 */
+	int GetFilmsNo(void) const { return filmsNo; }
+
+	/* @return: Na epistrfei thn domh pou exoume fila3ei thn pliroforia gia ta films
+	 */
+	const FilmsInfoMap GetFilmsInfo(void) const { return filmsInfo; }
+	
 	//to path gia to config file me ta data giata films
 	LoadFilmsInfo(const char* path);
 	~LoadFilmsInfo(void);

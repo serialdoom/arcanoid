3385fd5f81d481eddaadb578b7bcdef2e6e73890
apixkernel <apixkernel@5c4dd20e-9542-0410-abe3-ad2d610f3ba4>
2008-01-16 14:41:44 +0000

    BitmapLoader.cpp: assert (line 46)

--- trunk/Arcanoid/Arcanoid/BitmapLoader.cpp
+++ trunk/Arcanoid/Arcanoid/BitmapLoader.cpp
@@ -43,7 +43,7 @@ BITMAP * BitmapLoader::Load (const char* path) {
 	BITMAP * b = GetBitmap(path);
 	if (!b) {
 		bitmaps[path] = ( b = load_bitmap(path, NULL) );
-		assert(b);
+		assert(b); // error sthn ball.
 	}
 	return b;
 }
